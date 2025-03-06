cf4dcbcc9e7988ee76ba72aa8fd5440dd2160229
Julian331 <167152821+Julian331@users.noreply.github.com>
2025-03-06 09:59:18 -0500

    Create factorial_iterativo.c

---
+++ PRIMER PARCIAL - JULIAN CARO - ANGEL ROBLES/PUNTO 1/factorial_iterativo.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+
+unsigned long long factorial_iterativo(int n) {
+    unsigned long long resultado = 1;
+    for (int i = 1; i <= n; i++) {
+        resultado *= i;
+    }
+    return resultado;
+}
